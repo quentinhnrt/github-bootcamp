c68f290d9e5bf484dc10c4c4d48387cffdf2b7c8
Quentin Honnart <quentin.honnart@gmail.com>
2024-01-18 14:12:36 +0100

    feat(changelog.config.js): add config to commitizen

---
+++ changelog.config.js
@@ -0,0 +1,6 @@
+module.exports = {
+    list: ["test", "feat", "fix", "chore", "docs", "refactor"],
+    maxMessageLength: 64,
+    minMessageLength: 3,
+    questions: ["type", "subject", "breaking", "issues"],
+};
